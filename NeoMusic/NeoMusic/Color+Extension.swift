ba5ab801b78847b6a8ff7433609b8b887cb4dc9c
Jordan Christensen <jordan.c4922@gmail.com>
2020-05-13 07:46:39 -0600

    Add initial project, update storyboard

---
+++ NeoMusic/NeoMusic/Color+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  SwiftUI-MusicPlayer
+//
+//  Created by Jordan Christensen on 4/3/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let topGradientColor = UIColor(named: "BackgroundGradientTop")
+    static let bottomGradientColor = UIColor(named: "BackgroundGradientBottom")
+    static let buttonColor = UIColor(named: "Button")
+    static let buttonBackgroundColor = UIColor(named: "ButtonBackground")
+    static let playButtonLightColor = UIColor(named: "PlayDark")
+    static let playButtonDarkColor = UIColor(named: "PlayLight")
+    static let pauseButtonLightColor = UIColor(named: "PauseDark")
+    static let pauseButtonDarkColor = UIColor(named: "PauseLight")
+    static let trackYellowColor = UIColor(named: "TrackYellow")
+}
