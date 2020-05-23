6ba89a297964da8754be4a41a2e5a19253f2eae5
Jordan Christensen <jordan.c4922@gmail.com>
2020-05-22 20:30:31 -0600

    Add MusicPlayer class, add functions and variables to play, queue, pause, skip, etc. songs

---
+++ NeoMusic/NeoMusic/Model Controllers/MusicPlayer.swift
@@ -0,0 +1,118 @@
+//
+//  MusicPlayer.swift
+//  NeoMusic
+//
+//  Created by Jordan Christensen on 5/13/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import Foundation
+import MediaPlayer
+
+protocol MusicPlayerDelegate: class {
+    var musicPlayer: MusicPlayer? { get set }
+    func songUpdated(song: Song)
+    func currentTimeUpdated(time: String)
+    func playerStatusUpdated(isPlaying: Bool)
+}
+
+@objc
+class MusicPlayer: NSObject {
+    weak var delegate: MusicPlayerDelegate?
+    var queue = Queue<Song>()
+    
+    @objc let player = MPMusicPlayerController.applicationMusicPlayer
+    
+    var song: MPMediaItem? {
+        self.player.nowPlayingItem
+    }
+    
+    var isPlaying: Bool {
+        self.player.playbackState == .playing
+    }
+    
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(songChanged), name: .MPMusicPlayerControllerNowPlayingItemDidChange, object: self.player)
+        NotificationCenter.default.addObserver(self, selector: #selector(stateChanged), name: .MPMusicPlayerControllerPlaybackStateDidChange, object: self.player)
+        
+        player.setQueue(with: MPMediaItemCollection(items: getSongs()))
+        player.prepareToPlay()
+    }
+    
+    var currentTime: TimeInterval {
+        player.currentPlaybackTime
+    }
+    
+    func getSongs() -> [MPMediaItem] {
+        let albumsQuery = MPMediaQuery.albums()
+        guard let albumItems: [MPMediaItemCollection] = albumsQuery.collections else { return [] }
+        var songs: [MPMediaItem] = []
+
+        for album in albumItems {
+            let albumItems: [MPMediaItem] = album.items as [MPMediaItem]
+            songs += albumItems
+        }
+        return songs.shuffled()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func set(time: TimeInterval) {
+        player.currentPlaybackTime = time
+    }
+    
+    func toggle() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func skipBack() {
+        if player.currentPlaybackTime < 10 {
+            player.skipToPreviousItem()
+        } else {
+            player.currentPlaybackTime = 0
+        }
+    }
+    
+    func skipForward() {
+        player.skipToNextItem()
+    }
+    
+    @objc
+    func songChanged() {
+        if let media = player.nowPlayingItem, let delegate = delegate {
+            let song = Song(song: media)
+            delegate.songUpdated(song: song)
+        }
+    }
+    
+    @objc
+    func updateTime() {
+        if let delegate = delegate {
+            delegate.currentTimeUpdated(time: player.currentPlaybackTime.stringTime)
+        }
+    }
+    
+    @objc
+    func stateChanged() {
+        if let delegate = delegate {
+            let bool: Bool
+            if player.playbackState == .interrupted || player.playbackState == .playing {
+                bool = true
+            } else {
+                bool = false
+            }
+            delegate.playerStatusUpdated(isPlaying: bool)
+        }
+    }
+}
